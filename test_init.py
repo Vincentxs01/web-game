07475e8fb4225308b2ddf6226d635cdbb849bd03
Vincentxs01 <135814975+Vincentxs01@users.noreply.github.com>
2026-05-22 00:12:39 +0800

    Delete test_init.py

--- test_init.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["SDL_VIDEODRIVER"] = "dummy"
-os.environ["SDL_AUDIODRIVER"] = "dummy"
-import pygame
-pygame.init()
-from main import GameCoordinator
-print("Initializing GameCoordinator in headless mode...")
-coord = GameCoordinator()
-print("GameCoordinator initialized successfully!")
